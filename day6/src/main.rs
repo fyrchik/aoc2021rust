8fdcc5cb9b78484106ac660da887da493d85ad9c
Evgenii Stratonikov <stratonikov@runbox.com>
2021-12-06 18:21:14 +0300

    day6: use O(log(days)) algorithm
    Signed-off-by: Evgenii Stratonikov <stratonikov@runbox.com>

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -25,32 +25,56 @@ fn parse(input: &str) -> Result<Vec<u8>> {
     Ok(fishes)
 }
 
+fn prev(hist: &mut [usize]) {
+    // 1 at 0 result in [x0, x1, x2, x3, x4, x5, x6,    x7, x8] after n days
+    // 1 at 1 result in [x8, x0, x1, x2, x3, x4, x5, x6-x8, x7] after n days
+    //dbg!(&hist);
+    let x8 = hist[8];
+    hist.copy_within(0..=7, 1);
+    hist[0] = x8;
+    hist[7] = hist[7].saturating_sub(x8);
+}
+
 fn part1(fishes: &[u8], days: usize) -> usize {
     let mut hist = [0_usize; 9];
     for f in fishes.iter() {
         hist[*f as usize] += 1;
     }
 
-    for _ in 0..days/7 {
-        let baby0 = hist[7];
-        let baby1 = hist[8];
-        hist[7] = hist[5];
-        hist[8] = hist[6];
-        hist[5] += hist[3];
-        hist[6] += hist[4];
-        hist[3] += hist[1];
-        hist[4] += hist[2];
-        hist[2] += hist[0];
-        hist[0] += baby0;
-        hist[1] += baby1;
+    // zero contains 1 fish at 0 day before spawning.
+    let mut zero = [0_usize; 9];
+    zero[0] = 1;
+
+    for i in (0..=usize::BITS - days.leading_zeros()).rev() {
+        let mut tmp = [0_usize; 9];
+
+        // Calculate how much fish will spawn after 2*n days
+        // given zero = how much fish will spawn after n days.
+        let mut p = zero;
+        for i in zero.iter() {
+            for j in 0..9 {
+                tmp[j] += i * p[j];
+            }
+            prev(&mut p);
+        }
+
+        if days & (1 << (i as usize)) != 0 {
+            let first = tmp[0];
+            tmp.copy_within(1..=8, 0);
+            tmp[6] += first;
+            tmp[8] = first;
+        }
+        zero = tmp;
     }
-    for _ in 0..days%7 {
-        let first = hist[0];
-        hist.copy_within(1..=8, 0);
-        hist[6] += first;
-        hist[8] = first;
+
+    let mut tmp = [0_usize; 9];
+    for i in hist.iter() {
+        for j in 0..9 {
+            tmp[j] += i * zero[j];
+        }
+        prev(&mut zero);
     }
-    hist.iter().sum()
+    tmp.iter().sum()
 }
 
 #[cfg(test)]
@@ -58,12 +82,23 @@ mod tests {
     use crate::{parse, part1};
 
     #[test]
-    fn simple() {
+    fn zero_only() {
+        let input = "0";
+        let lines = parse(input).expect("can't parse the input");
+
+        assert_eq!(1, part1(&lines, 0));
+        assert_eq!(2, part1(&lines, 1));
+        assert_eq!(2, part1(&lines, 7));
+        assert_eq!(3, part1(&lines, 8));
+        assert_eq!(4, part1(&lines, 10));
+    }
+
+    #[test]
+    fn example() {
         let input = "3,4,3,1,2";
         let lines = parse(input).expect("can't parse the input");
 
         assert_eq!(26, part1(&lines, 18));
         assert_eq!(5934, part1(&lines, 80));
-        assert_eq!(26984457539, part1(&lines, 256));
     }
 }
