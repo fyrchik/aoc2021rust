a0fa9c24fc03a5997a3d26c3164e7db1fc07a35d
Evgenii Stratonikov <stratonikov@runbox.com>
2021-12-26 15:01:49 +0300

    day25: split main logic into functions
    Signed-off-by: Evgenii Stratonikov <stratonikov@runbox.com>

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -48,68 +48,76 @@ fn print_field(f: &[Vec<Cucumber>]) {
     }
 }
 
+fn move_east(field: &mut [Vec<Cucumber>]) -> bool {
+    let mut moved = false;
+    for row in field.iter_mut() {
+        let move_zero = row[0] == Cucumber::East && row[1] == Cucumber::Empty;
+        let last = row.len() - 1;
+
+        let mut j = 0;
+        while j < last {
+            if row[j] == Cucumber::East && row[j + 1] == Cucumber::Empty {
+                moved = true;
+                row[j] = Cucumber::Empty;
+                row[j + 1] = Cucumber::East;
+                j += 2;
+            } else {
+                j += 1;
+            }
+        }
+
+        if j == last && row[last] == Cucumber::East && row[0] == Cucumber::Empty && !move_zero {
+            moved = true;
+            row[last] = Cucumber::Empty;
+            row[0] = Cucumber::East;
+        }
+    }
+    moved
+}
+
+fn move_south(field: &mut [Vec<Cucumber>]) -> bool {
+    let mut moved = false;
+    for i in 0..field[0].len() {
+        let move_zero = field[0][i] == Cucumber::South && field[1][i] == Cucumber::Empty;
+        let last = field.len() - 1;
+
+        let mut j = 0;
+        while j < last {
+            if field[j][i] == Cucumber::South && field[j + 1][i] == Cucumber::Empty {
+                moved = true;
+                field[j][i] = Cucumber::Empty;
+                field[j + 1][i] = Cucumber::South;
+                j += 2;
+            } else {
+                j += 1;
+            }
+        }
+
+        if j == last
+            && field[last][i] == Cucumber::South
+            && field[0][i] == Cucumber::Empty
+            && !move_zero
+        {
+            moved = true;
+            field[last][i] = Cucumber::Empty;
+            field[0][i] = Cucumber::South;
+        }
+    }
+    moved
+}
+
 fn part1(input: &str) -> u32 {
     let mut field = parse(input);
 
-    let mut step = 0;
-    let mut moved = true;
-    while moved {
-        moved = false;
-
-        for row in field.iter_mut() {
-            let move_zero = row[0] == Cucumber::East && row[1] == Cucumber::Empty;
-            let last = row.len() - 1;
-
-            let mut j = 0;
-            while j < last {
-                if row[j] == Cucumber::East && row[j + 1] == Cucumber::Empty {
-                    moved = true;
-                    row[j] = Cucumber::Empty;
-                    row[j + 1] = Cucumber::East;
-                    j += 2;
-                } else {
-                    j += 1;
-                }
-            }
-
-            if j == last && row[last] == Cucumber::East && row[0] == Cucumber::Empty && !move_zero {
-                moved = true;
-                row[last] = Cucumber::Empty;
-                row[0] = Cucumber::East;
-            }
+    for step in 1.. {
+        let mut moved = false;
+        moved = move_east(&mut field) || moved;
+        moved = move_south(&mut field) || moved;
+        if !moved {
+            return step;
         }
-
-        for i in 0..field[0].len() {
-            let move_zero = field[0][i] == Cucumber::South && field[1][i] == Cucumber::Empty;
-            let last = field.len() - 1;
-
-            let mut j = 0;
-            while j < last {
-                if field[j][i] == Cucumber::South && field[j + 1][i] == Cucumber::Empty {
-                    moved = true;
-                    field[j][i] = Cucumber::Empty;
-                    field[j + 1][i] = Cucumber::South;
-                    j += 2;
-                } else {
-                    j += 1;
-                }
-            }
-
-            if j == last
-                && field[last][i] == Cucumber::South
-                && field[0][i] == Cucumber::Empty
-                && !move_zero
-            {
-                moved = true;
-                field[last][i] = Cucumber::Empty;
-                field[0][i] = Cucumber::South;
-            }
-        }
-
-        step += 1;
     }
-
-    step
+    unreachable!()
 }
 
 fn part2(_: &str) -> u32 {
